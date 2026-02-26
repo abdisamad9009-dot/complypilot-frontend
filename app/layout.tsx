3c0b97b6967aebbb50f8e771027491c5e2af0cdc
abdisamad9009-dot <abdisamad9009@gmail.com>
2026-02-26 09:13:22 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+     <body
+  className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
+>
       >
         {children}
       </body>
